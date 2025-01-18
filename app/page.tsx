5e1f91942deb292e257904500ae3269b898d5c52
Anna Yudina <annayudi@gmail.com>
2025-01-18 22:55:02 +0200

    Cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { EelLogo } from '@/components/EelLogo';
 
 export default function HomePage() {
     return (
-        <div className="min-h-screen bg-white dark:bg-gray-900 flex items-center justify-center w-screen">
+        <div className="min-h-screen flex items-center justify-center w-screen">
             <EelLogo />
         </div>
     );
